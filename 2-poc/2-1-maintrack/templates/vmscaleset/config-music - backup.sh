b53a7564dfb6c32a563a0b5923909f9e00f4068d
Rodrigo Santos <rosanto@microsoft.com>
2019-02-12 18:55:20 -0800

    updates

--- 2-poc/2-1-maintrack/templates/vmscaleset/config-music - backup.sh
+++ 2-poc/2-1-maintrack/templates/vmscaleset/config-music - backup.sh
@@ -4,18 +4,18 @@ bb="localadmin"
 cc="P@ssw0rd0101#"
 
 # 1 install dotnet core
-echo "-------------------------------------------------------- " >> /tmp/install.log
-echo "1 install dotnet core" >> /tmp/install.log
-sudo wget https://raw.githubusercontent.com/rodrigosantosms/aahc/master/2-poc/2-1-maintrack/templates/vmscaleset/dotnet-install.sh /tmp/  >> /tmp/install.log
-cd /tmp
-sudo ./dotnet-install.sh --channel LTS  >> /tmp/install.log
-
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-get update
+sudo apt-get install dotnet-dev-1.1.4
+echo "1 install dotnet core" >> /tmp/hello.txt
 
 # 2 download application
 sudo wget https://raw.github.com/Microsoft/dotnet-core-sample-templates/master/dotnet-core-music-linux/music-app/music-store-azure-demo-pub.tar /
 sudo mkdir /opt/music
 sudo tar -xf music-store-azure-demo-pub.tar -C /opt/music
-echo "2 download application" >> /tmp/install.log
+echo "2 download application" >> /tmp/hello.txt
 
 # 3 install nginx, update config file
 sudo apt-get install -y nginx
@@ -24,7 +24,7 @@ sudo touch /etc/nginx/sites-available/default
 sudo wget https://raw.githubusercontent.com/Microsoft/dotnet-core-sample-templates/master/dotnet-core-music-linux/music-app/nginx-config/default -O /etc/nginx/sites-available/default
 sudo cp /opt/music/nginx-config/default /etc/nginx/sites-available/
 sudo nginx -s reload
-echo "3 install nginx, update config file" >> /tmp/install.log
+echo "3 install nginx, update config file" >> /tmp/hello.txt
 
 # 4 update and secure music config file
 sed -i "s/<replaceserver>/$aa/g" /opt/music/config.json
@@ -32,7 +32,7 @@ sed -i "s/<replaceuser>/$bb/g" /opt/music/config.json
 sed -i "s/<replacepass>/$cc/g" /opt/music/config.json
 sudo chown $bb /opt/music/config.json
 sudo chmod 0400 /opt/music/config.json
-echo "4 update and secure music config file" >> /tmp/install.log
+echo "4 update and secure music config file" >> /tmp/hello.txt
 
 # 5 config supervisor
 sudo apt-get install -y supervisor
@@ -40,8 +40,8 @@ sudo touch /etc/supervisor/conf.d/music.conf
 sudo wget https://raw.githubusercontent.com/Microsoft/dotnet-core-sample-templates/master/dotnet-core-music-linux/music-app/supervisor/music.conf -O /etc/supervisor/conf.d/music.conf
 sudo service supervisor stop
 sudo service supervisor start
-echo "5 config supervisor" >> /tmp/install.log
+echo "5 config supervisor" >> /tmp/hello.txt
 
 # 6 pre-create music store database
 /usr/bin/dotnet /opt/music/MusicStore.dll &
-echo "6 pre-create music store database" >> /tmp/install.log
+echo "6 pre-create music store database" >> /tmp/hello.txt
