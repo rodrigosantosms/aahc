eb30d83773eef2da85d735d81df54186fc3a1312
Rodrigo Santos <rosanto@microsoft.com>
2019-02-14 19:54:56 -0800

    updates

--- 2-poc/2-1-maintrack/templates/vmscaleset/config-music.sh
+++ 2-poc/2-1-maintrack/templates/vmscaleset/config-music.sh
@@ -16,14 +16,14 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https >> /tmp/install.log
 sudo apt-get update && sudo apt-get install -y libicu52 libcurl3 liblldb-3.6 libunwind8-dev libunwind >> /tmp/install.log
 sudo apt-get install -y dotnet-dev-1.1.11 >> /tmp/install.log
-sudo wget -P /tmp https://download.visualstudio.microsoft.com/download/pr/c0957a2b-cac6-44d8-b1cc-0dad4420c825/8dc69e33f8cf44152fdf173d3bf0b746/dotnet-dev-ubuntu-x64.1.1.11.tar.gz  --append-output=/tmp/install.log
+sudo wget -O /tmp/dotnet-dev-ubuntu-x64.1.1.11.tar.gz https://download.visualstudio.microsoft.com/download/pr/c0957a2b-cac6-44d8-b1cc-0dad4420c825/8dc69e33f8cf44152fdf173d3bf0b746/dotnet-dev-ubuntu-x64.1.1.11.tar.gz  --append-output=/tmp/install.log
 sudo tar zxf dotnet-dev-ubuntu-x64.1.1.11.tar.gz -C /opt/dotnet
 dotnet --version  >> /tmp/install.log
 
 # Phase 2 - Download application
 echo "--------------------------------------------------------" >> /tmp/install.log
 echo "Phase 2 - Download application" >> /tmp/install.log
-sudo wget -P /tmp https://raw.githubusercontent.com/rodrigosantosms/aahc/master/2-poc/2-1-maintrack/templates/vmscaleset/music-store-azure-demo-pub.tar --append-output=/tmp/install.log
+sudo wget -O /tmp/music-store-azure-demo-pub.tar https://raw.githubusercontent.com/rodrigosantosms/aahc/master/2-poc/2-1-maintrack/templates/vmscaleset/music-store-azure-demo-pub.tar --append-output=/tmp/install.log
 sudo mkdir /opt/music
 sudo tar -xf /tmp/music-store-azure-demo-pub.tar -C /opt/music
 
@@ -34,7 +34,7 @@ sudo apt-get install -y nginx >> /tmp/install.log
 sudo service nginx start >> /tmp/install.log
 sudo touch /etc/nginx/sites-available/default >> /tmp/install.log
 sudo cp /etc/nginx/sites-available/default /tmp/bkp_nginx_sites-available_default >> /tmp/install.log
-sudo wget -P /tmp https://raw.githubusercontent.com/Microsoft/dotnet-core-sample-templates/master/dotnet-core-music-linux/music-app/nginx-config/default --append-output=/tmp/install.log
+sudo wget -O /tmp/default https://raw.githubusercontent.com/Microsoft/dotnet-core-sample-templates/master/dotnet-core-music-linux/music-app/nginx-config/default --append-output=/tmp/install.log
 sudo mkdir /opt/music/nginx-config >> /tmp/install.log
 sudo cp /tmp/default /opt/music/nginx-config/default >> /tmp/install.log
 sudo cp /tmp/default /etc/nginx/sites-available/default >> /tmp/install.log
@@ -46,15 +46,15 @@ echo "Phase 4 - Update and secure music config file" >> /tmp/install.log
 sudo sed -i "s/<replaceserver>/$aa/g" /opt/music/config.json >> /tmp/install.log
 sudo sed -i "s/<replaceuser>/$bb/g" /opt/music/config.json >> /tmp/install.log
 sudo sed -i "s/<replacepass>/$cc/g" /opt/music/config.json >> /tmp/install.log
-sudo chown $bb /opt/music/config.json >> /tmp/install.log
-sudo chmod 0400 /opt/music/config.json >> /tmp/install.log
+sudo chown $bb /opt/music/config.json
+sudo chmod 0400 /opt/music/config.json
 
 # 5 config supervisor
 echo "--------------------------------------------------------" >> /tmp/install.log
 echo "Phase 5 - Config supervisor" >> /tmp/install.log
 sudo apt-get install -y supervisor
 sudo touch /etc/supervisor/conf.d/music.conf >> /tmp/install.log
-sudo wget -P /etc/supervisor/conf.d/music.conf https://raw.githubusercontent.com/Microsoft/dotnet-core-sample-templates/master/dotnet-core-music-linux/music-app/supervisor/music.conf --append-output=/tmp/install.log
+sudo wget -O /etc/supervisor/conf.d/music.conf https://raw.githubusercontent.com/Microsoft/dotnet-core-sample-templates/master/dotnet-core-music-linux/music-app/supervisor/music.conf --append-output=/tmp/install.log
 sudo service supervisor stop >> /tmp/install.log
 sudo service supervisor start >> /tmp/install.log
 
@@ -62,7 +62,7 @@ sudo service supervisor start >> /tmp/install.log
 echo "--------------------------------------------------------" >> /tmp/install.log
 echo "Phase 6 - Pre-create music store database" >> /tmp/install.log
 dotnet --version >> /tmp/dotnetversion.log
-sudo wget -P /etc/init.d/musicstorestopstart.sh https://raw.githubusercontent.com/rodrigosantosms/aahc/master/2-poc/2-1-maintrack/templates/vmscaleset/musicstorestopstart.sh --append-output=/tmp/install.log
-sudo chmod a+x /etc/init.d/musicstorestopstart.sh
+sudo wget -O /etc/init.d/musicstorestopstart.sh https://raw.githubusercontent.com/rodrigosantosms/aahc/master/2-poc/2-1-maintrack/templates/vmscaleset/musicstorestopstart.sh --append-output=/tmp/install.log
+chmod a+x /etc/init.d/musicstorestopstart.sh
 sudo update-rc.d musicstorestopstart.sh defaults
 sudo /etc/init.d/musicstorestopstart.sh start
